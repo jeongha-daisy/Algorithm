e3f2b8932653d193962fbaca45754c6a769f2a91
jeongha-daisy <jeongha.daisy@gmail.com>
2025-11-09 21:31:01 +0900

    [BOJ] 1926 풀이

---
+++ BOJ/BFS/1926.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// 전역 변수
+int paper[501][501] = {};
+bool visited[501][501] = {};
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+int biggest = 0;
+
+int BFS(int n, int m, int i, int j)
+{
+    int size = 0;
+    queue<pair<int, int>> q;
+
+    q.push(make_pair(i, j));
+    visited[i][j] = true;
+    size++;
+
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 || nx < n || ny >= 0 || ny < m)
+            {
+                if (paper[nx][ny] == 1 && !visited[nx][ny])
+                {
+                    q.push(make_pair(nx, ny));
+                    visited[nx][ny] = true;
+                    size++;
+                }
+            }
+        }
+    }
+
+    return size;
+}
+
+void solution(int n, int m)
+{
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (paper[i][j] == 1 && !visited[i][j])
+            {
+                biggest = max(biggest, BFS(n, m, i, j));
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl
+         << biggest;
+}
+
+int main()
+{
+    // 입출력 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int paint;
+            cin >> paint;
+            paper[i][j] = paint;
+        }
+    }
+    solution(n, m);
+    return 0;
+}
